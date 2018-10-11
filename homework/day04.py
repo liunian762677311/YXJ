8ffcb8a7669f4867267de42cbeeda352a75bab48
root <root@localhost.localdomain>
2018-10-11 10:07:47 +0800

    homework

---
+++ homework/day04.py
@@ -0,0 +1,158 @@
+# _*_ coding:utf8-
+from math import *
+from random import *
+
+
+'''
+#1.数学方面: 五角数
+def getpen(n):
+    n=n*(3*n-1)/2
+    print("%.0f"%(n),end=" ")
+s=0
+for i in range(1,101):
+    getpen(i)
+    s=s+1
+    if(s%10==0):
+        print('')
+'''
+
+
+
+'''
+#2.求一个整数各个数字的和
+def sumdigits(n):
+    s=0
+    while(n%10!=0):
+        a=n%10
+        b=n//10
+        s=s+a
+        n=b
+    print(s)
+a=eval(input("Enter a int:"))
+sumdigits(a)
+'''
+
+
+
+'''
+#3.对三个数排序
+def displaysort(num1,num2,num3):
+    b=[num1,num2,num3]
+    b.sort()
+    print(b)
+    
+a,b,c=eval(input("Enter three numbers:"))
+displaysort(a,b,c)
+'''
+from math import *
+
+
+'''
+#4.计算未来投资值
+invested=eval(input("The amount inversted:"))
+monthly=eval(input("Annual interest rate:"))
+print("Annual\tFuture Value")
+def funtureinver(invested,monthly,years):
+    return invested*pow((1+monthly/100/12),years*12)
+for i in range(1,31):
+    c=funtureinver(invested,monthly,i)
+    print("%d\t%.2f"%(i,c),end="  ")
+    print()
+'''
+
+
+
+
+
+'''
+#5.显示字符
+def printchars(ch1,ch2,number):
+    m=0
+    a=ord(ch1)
+    b=ord(ch2)+1
+    for i in range(a,b):
+        print(chr(i),end=" ")
+        m=m+1
+        if(m%number==0):
+            print(" ")
+a,b=input("Enter start to end ascii:").split(',')
+c=eval(input("Enter number:"))
+printchars('1','Z',10)
+'''
+
+
+'''
+#6.一年的天数
+def numberofdays(year):
+    if((year%4==0)&(year%100!=0))|(year%400==0):
+        print("%d:366"%(i))
+    else:
+        print("%d:365"%(i))
+for i in range(2010,2021):
+    numberofdays(i)
+'''
+
+'''
+#7.几何问题：显示角
+def distance(x1,y1,x2,y2):
+    print(((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))**0.5)
+x1,y1=eval(input("Enter x1 and x2 for point1: "))
+x2,y2=eval(input("Enter x1 and x2 for point2: "))
+distance(x1,y1,x2,y2)
+'''
+
+
+
+#8.梅森素数
+print("p\t2^p-1:")
+def s(a):
+    c=0
+    for j in range(2,int(sqrt(a)+1)):
+            if a%j==0:
+                c=0
+            else:
+                c=1
+    return c
+print("2\t3")
+for i in range(1,32):
+    c=pow(2,i)-1
+    if(s(c)):
+        print("%d\t%d"%(i,c))
+
+    
+
+
+'''
+#9.当前时间和日期
+from time import *
+print(ctime(time()))
+'''
+
+
+
+'''
+#10.游戏：掷骰子
+a,b=eval(input("Enter one  and two:"))
+if(a+b==2)|(a+b==3)|(a+b==12):
+    print("You rolled %d+%d=%d"%(a,b,a+b))
+    print("You lose")
+elif(a+b==7)|(a+b==11):
+    print("You rolled %d+%d=%d"%(a,b,a+b))
+    print("You win")
+else:
+    while(1):
+        print("You rolled %d+%d=%d"%(a,b,a+b))
+        print("print is %d"%(a+b))
+        s=a+b
+        a,b=eval(input("Enter one  and two:"))
+        if(a+b==7):
+            print("You rolled %d+%d=%d"%(a,b,a+b))
+            print("You lose")
+            break
+        elif(a+b==s):
+            print("You rolled %d+%d=%d"%(a,b,s))
+            print("You win")
+            break
+        else:
+           continue
+'''
--- homework/day05.py
+++ homework/day05.py
@@ -3,7 +3,7 @@ from math import *
 from random import *
 
 '''
-#Rectangle类
+#1.Rectangle类
 class Rectangle:
 	width=0
 	height=0
@@ -21,36 +21,131 @@ a.getPerimeter()
 '''
 
 
-#Account类
+'''
+#2.利息
+class account:
+    def __init__(self,id=0,balance=100,annual=0):
+        self.__id=int(id)
+        self.__balance=float(balance)
+        self.__annual=float(annual)
+        print(self.__id)
+    def withdraw(self,money):  #取出的金额
+        if (money<self.__balance):
+            self.w=self.__balance-money
+            print("余额:{}".format(self.w))
+        else:
+            print("没有那么多钱")
+    def deposit(self,ins):  #存入金额
+        self.d=ins+self.__balance
+        print("共有{}".format(self.d))
+    def getrate(self):   #月利率
+        self.y=self.__annual/100/12
+        print(self.y)
+    def get(self):  #月利息额
+        self.m=self.__balance*(self.__annual/100/12)
+        print(self.m)
+m=account(1122,20000,4.5)
+m.withdraw(2500)
+m.deposit(3000)
+m.get()
+m.getrate()
+'''
+'''
+#3.几何：正n边形
+from math import *
+class regular:
+    #输入正多边形的边数，长度，x轴的坐标，y轴的坐标，求出它的周长和面积
+    def __init__(self,n=3,side=1,x=0,y=0):
+        self.n=int(n)
+        self.side=float(side)
+        self.x=float(x)
+        self.y=float(y)
+    def getper(self):
+        print("周长为：%d"%(self.n*self.side))
+    def getarea(self):
+        area=(self.n*self.side*self.side)/(4*tan(pi/self.n))
+        print("面积为：%.2f"%(area))
+regular().getper()
+regular().getarea()
+regular(6,4).getper()
+regular(6,4).getarea()
+regular(10,4,5.6,7.8).getper()
+regular(10,4,5.6,7.8).getarea()
+'''
+'''
+#4.fan(风扇)
+class fan:
+    def __init__(self,speed=1,on=False,radius=5,color='blue'):
+        self.speed=int(speed)
+        self.on=bool(on)
+        self.radius=float(radius)
+        self.color=str(color)
+        print("speed is {},radius is {},color is {} and on is {}.".format(self.speed,self.radius,self.color,self.on))
+slow=1
+medium=2
+fast=3
+fan(fast,True,10,'yellow')
+fan(medium,False,5,'blue')
+'''
+'''
+#5.2x2线性方程式
+#输入a,b,c,d,e,f,根据公式求出x,y两个解
+class linear:
+    def __init__(self,a,b,c,d,e,f):
+        self.__a=a
+        self.__b=b
+        self.__c=c
+        self.__d=d
+        self.__e=e
+        self.__f=f
+    def issolvable(self):
+        self.o=(self.__a*self.__d)-(self.__b*self.__c)
+        if(self.o!=0):
+            return True
+        else:
+            print("方程无解")
+    def getx(self):
+        self.x=((self.__e*self.__d)-(self.__b*self.__f))/self.o
+        print("x={}".format(self.x))
+    def gety(self):
+        self.y=((self.__a*self.__f)-(self.__e*self.__c))/self.o
+        print("y={}".format(self.y))
+a,b,c,d,e,f=eval(input(">>"))
+k=linear(a,b,c,d,e,f)
+if(k.issolvable()==True):
+    k.getx()
+    k.gety()
+'''
 
+#6.几何：交叉线
+#kx+b+y=0
+class jiaocha:
+    def __init__(self,x1,y1,x2,y2,x3,y3,x4,y4):
+        self.__x1=x1
+        self.__y1=y1
+        self.__x2=x2
+        self.__y2=y2
+        self.__x3=x3
+        self.__y3=y3
+        self.__x4=x4
+        self.__y4=y4
+    def result(self):
+        k1=(self.__y2-self.__y1)/(self.__x2-self.__x1)*1.0
+        b1=self.__y1*1.0-self.__x1*k1*1.0
+        if(self.__x4-self.__x3)==0:
+            k2=None
+            b2=0
+        else:
+            k2=(self.__y4-self.__y3)*1.0/(self.__x4-self.__x3)
+            b2=self.__y3*1.0-self.__x3*k2*1.0
+        if k2==None:
+            x=self.__x3
+        else:
+            x=(b2-b1)*1.0/(k1-k2)
+        y=k1*x*1.0+b1*1.0
+        print(x,y)
 
-
-
-
-
-#正n边形
-
-
-
-
-
-
-#fan类
-
-
-
-
-
-
-#2x2线性方程
-
-
-
-
-
-
-
-#交叉线
+jiaocha(2,2,0,0,0,2,2,0).result()
 
 
 
